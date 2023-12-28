20e6fc0bda5ffd28c1ebcf61268ddc87edfd8273
Bibek <vivekregmi83@gmail.com>
2023-12-28 09:50:41 +0545

    Email verification added

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,10 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('verified');
+
+Auth::routes(['verify' => true]);
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
